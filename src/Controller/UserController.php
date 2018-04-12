9b78f149d3400d53987bb63b8476767ad5a345a3
Thibaud Crespin <thibaud.crespin@biig.fr>
2018-04-12 19:00:25 +0200

    Add form and translations

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,107 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class UserController extends Controller
 {
     /**
      * @Route("/user", name="user")
      */
-    public function index()
+    public function index($id)
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'No user found for id '.$id
+            );
+        }
+
+        return $this->render('user/index.html.twig', array(
+            'user' => $user,
+        ));
+    }
+
+    /**
+     * @Route("/user/success", name="user_success")
+     * @Template()
+     */
+    public function success()
+    {
+        return array('message' => 'User successfully created.');
+    }
+
+    /**
+     * @Route("/user/new", name="user_new")
+     * @Template()
+     */
+    public function new(Request $request, TranslatorInterface $translator)
+    {
+        $user = new User();
+
+        $form = $this->createFormBuilder($user)
+            ->add('firstName', TextType::class, array(
+                'label' => $translator->trans('form.firstName')
+            ))
+            ->add('lastName', TextType::class, array(
+                'label' => $translator->trans('form.lastName')
+            ))
+            ->add('mail', EmailType::class, array(
+                'label' => $translator->trans('form.mail')
+            ))
+            ->add('birth', BirthdayType::class, array(
+                'label' => $translator->trans('form.birth')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => $translator->trans('form.save')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('user_success');
+        }
+
+        return array(
+            'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * @Route("/users", name="user_list")
+     * @Template()
+     */
+    public function list()
+    {
+        $users = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findAll();
+
+        if (!$users) {
+            throw $this->createNotFoundException(
+                'No users found'
+            );
+        }
+
+        return array('users' => $users);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -18,21 +19,48 @@ class User
 
     /**
      * @ORM\Column(type="string", length=128)
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 128,
+     *      minMessage = "Your first name must be at least {{ limit }} characters long",
+     *      maxMessage = "Your first name cannot be longer than {{ limit }} characters"
+     * )
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=128)
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *      min = 2,
+     *      max = 128,
+     *      minMessage = "Your last name must be at least {{ limit }} characters long",
+     *      maxMessage = "Your last name cannot be longer than {{ limit }} characters"
+     * )
      */
     private $lastName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *      min = 5,
+     *      max = 255,
+     *      minMessage = "Your email must be at least {{ limit }} characters long",
+     *      maxMessage = "Your email cannot be longer than {{ limit }} characters"
+     * )
+     * @Assert\Email(
+     *     message = "The email '{{ value }}' is not a valid email.",
+     *     strict = true
+     * )
      */
     private $mail;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Assert\NotBlank()
+     * @Assert\Type("\DateTime")
      */
     private $birth;
 
